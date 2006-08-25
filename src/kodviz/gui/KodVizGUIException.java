5923681570ade0e4d47013ef54dae305213a2470
Felix Chang <fschang@82c6cff8-24fa-0310-a8e5-8c849366ca4b>
2006-08-25 04:43:02 +0000

    The commit message was empty

--- src/kodviz/gui/KodVizGUIException.java
+++
@@ -1,7 +0,0 @@
-package kodviz.gui;
-
-public class KodVizGUIException extends Exception {
-
-	private static final long serialVersionUID = 1L;
-
-}
--- src/kodviz/util/Version.java
+++
@@ -1,3 +0,0 @@
-package kodviz.util;
-public class Version { 
-public static final String BUILD_DATE=" Wed Aug 2 14:01:05 EDT 2006 "; }
