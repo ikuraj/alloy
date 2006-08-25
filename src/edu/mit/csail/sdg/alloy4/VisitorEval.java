c31f9cc952065e491e3cdba17628ec8798e7b541
Felix Chang <fschang@82c6cff8-24fa-0310-a8e5-8c849366ca4b>
2006-08-25 15:34:26 +0000

    The commit message was empty

--- src/edu/mit/csail/sdg/alloy4/VisitorEval.java
+++ src/edu/mit/csail/sdg/alloy4/VisitorEval.java
@@ -821,9 +821,9 @@ Code generation
       try {
         Formula f=((Formula)(e.value.accept(this))).not().and(kfact);
         Solver solver = new Solver();
-        solver.options().setSolver(SATFactory.DefaultSAT4J);
+        //solver.options().setSolver(SATFactory.DefaultSAT4J);
         //solver.options().setSolver(SATFactory.MiniSat);
-        //solver.options().setSolver(SATFactory.ZChaffBasic);
+        solver.options().setSolver(SATFactory.ZChaffBasic);
         solver.options().setBitwidth(bitwidth);
         solver.options().setIntEncoding(Options.IntEncoding.BINARY);
         log.log0("Bitwidth="+bitwidth+" Checking \""+e.name+"\"...\t ");
@@ -865,9 +865,9 @@ Code generation
       try {
         Formula f=((Formula)(v.accept(this))).and(kfact);
         Solver solver = new Solver();
-        solver.options().setSolver(SATFactory.DefaultSAT4J);
+        //solver.options().setSolver(SATFactory.DefaultSAT4J);
         //solver.options().setSolver(SATFactory.MiniSat);
-        //solver.options().setSolver(SATFactory.ZChaffBasic);
+        solver.options().setSolver(SATFactory.ZChaffBasic);
         solver.options().setBitwidth(bitwidth);
         solver.options().setIntEncoding(Options.IntEncoding.BINARY);
         log.log0("Bitwidth="+bitwidth+" Running \""+e.name+"\"...\t ");
