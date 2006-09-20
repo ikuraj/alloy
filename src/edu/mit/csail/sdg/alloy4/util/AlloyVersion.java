2d76be5fae7c9137de91c9f925c3f28cb6ef43d8
Felix Chang <fschang@82c6cff8-24fa-0310-a8e5-8c849366ca4b>
2006-09-20 16:49:23 +0000

    The commit message was empty

--- src/edu/mit/csail/sdg/alloy4/util/AlloyVersion.java
+++ src/edu/mit/csail/sdg/alloy4/util/AlloyVersion.java
@@ -11,5 +11,5 @@ package edu.mit.csail.sdg.alloy4.util;
 public final class AlloyVersion {
 	
 	/** Returns the version String. */
-	public static String version() { return "Alloy version 4: latest development branch."; }
+	public static String version() { return "Alloy version 4: latest development branch"; }
 }
