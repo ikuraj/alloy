18c20decec1c9665ecd4d89acf4f06f7db1080cc
Felix Chang <fschang@82c6cff8-24fa-0310-a8e5-8c849366ca4b>
2006-11-13 02:36:55 +0000

    The commit message was empty

--- src/edu/mit/csail/sdg/alloy4/Pos.java
+++ src/edu/mit/csail/sdg/alloy4/Pos.java
@@ -19,6 +19,12 @@ public final class Pos {
     /** The row position (from 1..) */
     public final int y;
 
+    /** The column position (from 1..) */
+    public final int x2;
+
+    /** The row position (from 1..) */
+    public final int y2;
+
     /**
      * Constructs a new Pos object.
      * @param filename - the filename (it can be an empty string if unknown)
@@ -29,5 +35,25 @@ public final class Pos {
         this.filename=(filename==null?"":filename);
         this.x=(x>0?x:1);
         this.y=(y>0?y:1);
+        this.x2=this.x;
+        this.y2=this.y;
+    }
+
+    /**
+     * Constructs a new Pos object.
+     * @param filename - the filename (it can be an empty string if unknown)
+     * @param x - the starting column position (from 1..)
+     * @param y - the starting row position (from 1..)
+     * @param x2 - the ending column position (from 1..)
+     * @param y2 - the ending row position (from 1..)
+     */
+    public Pos(String filename, int x, int y, int x2, int y2) {
+        this.filename=(filename==null?"":filename);
+        this.x=(x>0?x:1);
+        this.y=(y>0?y:1);
+        if (y2<(this.y)) y2=this.y;
+        if (y2==this.y) { if (x2<(this.x)) x2=this.x; } else { if (x2<1) x2=1; }
+        this.x2=x2;
+        this.y2=y2;
     }
 }
