689abe5b127aa20a9d20d0557ca5428c2c83109c
Felix Chang <fschang@82c6cff8-24fa-0310-a8e5-8c849366ca4b>
2006-12-06 06:03:51 +0000

    The commit message was empty

--- src/edu/mit/csail/sdg/alloy4/Version.java
+++ src/edu/mit/csail/sdg/alloy4/Version.java
@@ -4,7 +4,7 @@ package edu.mit.csail.sdg.alloy4;
  * This class holds the buildDate String.
  *
  * The release build script
- * will generate a customized Version.java with the correct date.
+ * will generate a customized Version.java with the correct buildnumber and date.
  *
  * <p><b>Thread Safety:</b>  Safe.
  */
@@ -13,6 +13,9 @@ public final class Version {
 
     /** The constructor is private, since this class never needs to be instantiated. */
     private Version() { }
+
+    /** Returns the build number. */
+    public static int buildNumber() { return 0; }
 
     /** Returns the version string. */
     public static String version() { return "4.0"; }
