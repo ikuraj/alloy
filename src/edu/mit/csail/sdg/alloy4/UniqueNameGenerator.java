72a93d004f1bd6ed09a8667ada275dfd82ce5c16
Felix Chang <fschang@82c6cff8-24fa-0310-a8e5-8c849366ca4b>
2007-03-08 16:04:27 +0000

    The commit message was empty

--- src/edu/mit/csail/sdg/alloy4/UniqueNameGenerator.java
+++ src/edu/mit/csail/sdg/alloy4/UniqueNameGenerator.java
@@ -18,25 +18,21 @@ public final class UniqueNameGenerator {
     public UniqueNameGenerator() { }
 
     /**
+     * Regard the provided name as "seen".
+     * <p> For convenience, it returns the argument as the return value.
+     */
+    public synchronized String seen(String name) { names.add(name); return name; }
+
+    /**
      * Generate a unique name based on the input name.
      *
-     * <p> We first remove any '$' character at the beginning of the name.
-     * If the name was empty, or consisted only of '$' character, then we replace name with "x".
-     *
-     * <p> Then, if the resulting name has not been generated already by this generator,
+     * <p> Specifically: if the name has not been generated/seen already by this generator,
      * then it is returned as is. Otherwise, we append characters to it
      * until the name becomes unique.
      */
     public synchronized String make(String name) {
-        while(name.length()>0 && name.charAt(0)=='$') {
-            name=name.substring(1);
-        }
-        if (name.length()==0) {
-            name="x";
-        }
-        while(names.contains(name)) {
-            name=name+"'";
-        }
+        if (name.length()==0) name="x";
+        while(names.contains(name)) name=name+"'";
         names.add(name);
         return name;
     }
