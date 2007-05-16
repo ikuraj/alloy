92e121b731708033819d0ad5a3b516b7d0436b2b
Felix Chang <fschang@82c6cff8-24fa-0310-a8e5-8c849366ca4b>
2007-05-16 15:12:31 +0000

    The commit message was empty

--- src/edu/mit/csail/sdg/alloy4/SafeList.java
+++ src/edu/mit/csail/sdg/alloy4/SafeList.java
@@ -14,7 +14,8 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA,
+ * 02110-1301, USA
  */
 
 package edu.mit.csail.sdg.alloy4;
@@ -263,7 +264,7 @@ public final class SafeList<T> implements Collection<T>, Serializable {
             answer[i]=(S)obj;
             i++;
         }
-        while(i<n) {
+        while(i < answer.length) {
             answer[i]=null;
             i++;
         }
