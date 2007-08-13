4ce5a2084dfbbc0713660f130275239caa1710a0
Felix Chang <fschang@82c6cff8-24fa-0310-a8e5-8c849366ca4b>
2007-08-13 02:20:40 +0000

    The commit message was empty

--- src/edu/mit/csail/sdg/alloy4compiler/ast/ExprBuiltin.java
+++ src/edu/mit/csail/sdg/alloy4compiler/ast/ExprBuiltin.java
@@ -47,6 +47,8 @@ public final class ExprBuiltin extends Expr {
     /** Caches the span() result. */
     private Pos span=null;
 
+    //============================================================================================================//
+
     /** {@inheritDoc} */
     @Override public Pos span() {
         Pos p=span;
@@ -58,11 +60,13 @@ public final class ExprBuiltin extends Expr {
         return p;
     }
 
+    //============================================================================================================//
+
     /** {@inheritDoc} */
     @Override public void toString(StringBuilder out, int indent) {
         if (indent<0) {
             out.append("disjoint[");
-            for(int i=0; i<args.size(); i++) { if (i>0) out.append(", "); out.append(args.get(i)); }
+            for(int i=0; i<args.size(); i++) { if (i>0) out.append(", "); args.get(i).toString(out,-1); }
             out.append(']');
         } else {
             for(int i=0; i<indent; i++) { out.append(' '); }
@@ -71,11 +75,15 @@ public final class ExprBuiltin extends Expr {
         }
     }
 
+    //============================================================================================================//
+
     /** Constructs an ExprBuiltin node. */
     private ExprBuiltin(Pos pos, boolean ambiguous, Type type, ConstList<Expr> args, long weight, JoinableList<Err> errs) {
         super(pos, ambiguous, type, 0, weight, errs);
         this.args = args;
     }
+
+    //============================================================================================================//
 
     /** Generates the expression disj[arg1, args2, arg3...] */
     public static Expr makeDISJOINT(Pos pos, List<Expr> args) {
@@ -86,26 +94,29 @@ public final class ExprBuiltin extends Expr {
         long weight=0;
         if (args.size()<2)
             errs=errs.append(new ErrorSyntax(pos, "The builtin disjoint[] predicate must be called with at least two arguments."));
-        for(Expr a:args) {
+        for(int i=0; i<args.size(); i++) {
+            Expr a = cset(args.get(i));
             ambiguous = ambiguous || a.ambiguous;
             weight = weight + a.weight;
             if (a.mult!=0) errs = errs.append(new ErrorSyntax(a.span(), "Multiplicity expression not allowed here."));
-            Expr b=cset(a);
-            if (!b.errors.isEmpty()) { type=EMPTY; errs = errs.join(b.errors); }
-            else if (commonArity==null) commonArity=b.type;
-            else commonArity=commonArity.intersect(b.type);
-            newargs.add(b);
+            if (!a.errors.isEmpty()) { type=EMPTY; errs=errs.join(a.errors); }
+               else if (commonArity==null) commonArity=a.type;
+               else commonArity=commonArity.pickCommonArity(a.type);
+            newargs.add(a);
         }
-        if (commonArity!=null && commonArity==EMPTY) errs=errs.append(new ErrorType(pos,
+        if (commonArity==EMPTY) errs=errs.append(new ErrorType(pos,
            "The builtin predicate disjoint[] cannot be used among expressions of different arities."));
         return new ExprBuiltin(pos, ambiguous, type, newargs.makeConst(), weight, errs);
     }
 
+    //============================================================================================================//
+
     /** {@inheritDoc} */
     @Override public Expr resolve(Type p, Collection<ErrorWarning> warns) {
+        if (errors.size()>0) return this;
         p=EMPTY;
-        for(int i=0; i<this.args.size(); i++) {
-            if (i==0) p=this.args.get(i).type; else p=p.unionWithCommonArity(this.args.get(i).type);
+        for(int i=0; i<args.size(); i++) {
+            if (i==0) p=args.get(i).type; else p=p.unionWithCommonArity(args.get(i).type);
         }
         TempList<Expr> args = new TempList<Expr>(this.args.size());
         boolean changed = false;
@@ -114,9 +125,14 @@ public final class ExprBuiltin extends Expr {
             if (x!=y) changed=true;
             args.add(y);
         }
-        if (changed) return makeDISJOINT(pos, args.makeConst()); else return this;
+        return changed ? makeDISJOINT(pos, args.makeConst()) : this;
     }
 
+    //============================================================================================================//
+
     /** Accepts the return visitor. */
-    @Override Object accept(VisitReturn visitor) throws Err { return visitor.visit(this); }
+    @Override Object accept(VisitReturn visitor) throws Err {
+        if (!errors.isEmpty()) throw errors.get(0);
+        return visitor.visit(this);
+    }
 }
