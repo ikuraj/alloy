27c849d21ec59bce02d7e75e1038dd9a9b389352
Felix Chang <fschang@82c6cff8-24fa-0310-a8e5-8c849366ca4b>
2008-05-13 04:11:29 +0000

    Modified the VizCustomization panel to hide the controls for children of leaf sigs.

--- src/edu/mit/csail/sdg/alloy4viz/VizCustomizationPanel.java
+++ src/edu/mit/csail/sdg/alloy4viz/VizCustomizationPanel.java
@@ -259,11 +259,12 @@ public final class VizCustomizationPanel extends JPanel {
             if (s.equals(lastElement)) last=path.pathByAddingChild(rad2);
           }
         // Generate the nodes for all AlloyType that inherit from this
-        for(AlloyType t:old.getDirectSubTypes(type))
-          if (!(hidePrivate && t.isPrivate) && !(hideMeta && t.isMeta)) {
-            TreePath possibleLast=remakeForType(hidePrivate, hideMeta, path, t);
-            if (possibleLast!=null) last=possibleLast;
-          }
+        if (!type.isLeaf)
+          for(AlloyType t:old.getDirectSubTypes(type))
+            if (!(hidePrivate && t.isPrivate) && !(hideMeta && t.isMeta)) {
+              TreePath possibleLast=remakeForType(hidePrivate, hideMeta, path, t);
+              if (possibleLast!=null) last=possibleLast;
+            }
         return last;
     }
 
