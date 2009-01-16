f271bdd0fed2ffa8a8deb8ed7c72cf58dc31f544
Felix Chang <fschang@82c6cff8-24fa-0310-a8e5-8c849366ca4b>
2009-01-16 21:16:10 +0000

    The commit message was empty

--- src/edu/mit/csail/sdg/alloy4whole/SimpleGUI.java
+++ src/edu/mit/csail/sdg/alloy4whole/SimpleGUI.java
@@ -755,6 +755,7 @@ public final class SimpleGUI implements ComponentListener, Listener {
 
     /** This method performs File->Quit. */
     private Runner doQuit() {
+        System.out.println("Q"); System.out.flush();
         if (!wrap) if (text.closeAll()) {
             try { WorkerEngine.stop(); } finally { System.exit(0); }
         }
